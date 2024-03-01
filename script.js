d603176a07586f57b62e89036ec21fe5f34392d3
craignelstead <ctnelst92@gmail.com>
2024-03-01 14:50:47 -0700

    Created linked list and node factory function outlines

---
+++ script.js
@@ -0,0 +1,88 @@
+//Factory function for linked list
+const LinkedList = () => {
+
+    //Adds node to end of list
+    function append (value) {
+
+    }
+
+    //Adds node to beginning of list (becomes head)
+    function prepend (value) {
+
+    }
+
+    //Returns number of nodes in list
+    function size () {
+
+    }
+
+    //Returns first node in list
+    function head () {
+
+    }
+
+    //Returns last node in list
+    function tail () {
+
+    }
+
+    //Returns node at given index
+    function at (index) {
+
+    }
+
+    //Removes last element from list
+    function pop () {
+
+    }
+
+    //Returns true or false based on if value is in list
+    function contains (value) {
+
+    }
+
+    //Returns index of node containing value or null if not found
+    function find () {
+
+    }
+
+    //Represents linked list objects as strings
+    function toString () {
+
+    }
+
+    //Inserts new node at given index
+    function insertAt (value, index) {
+
+    }
+
+    //Removes node at given index
+    function removeAt (index) {
+
+    }
+
+    return {
+
+    }
+}
+
+
+//Factory function for nodes
+const Node = (data) => {
+
+    data = data;
+    next = 'Next';
+
+    return {
+        data,
+        next,
+    }
+}
+
+
+//Establishes data
+const initializer = (function() {
+
+
+
+})();
